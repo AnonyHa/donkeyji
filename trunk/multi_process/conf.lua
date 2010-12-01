7d900dfc6dee4f02754df7b227b40e554ff50b99
donkeyji <donkeyji@7faa1df5-b907-cf65-c270-3e1877ae6e1a>
2010-12-01 05:50:00 +0000

    prepare to implement a cgi server

---
+++ trunk/multi_process/conf.lua
@@ -0,0 +1,12 @@
+server = {
+	['network'] = {
+		['port'] = 10031,
+		['max_fds'] = 4096,
+	},
+	['log'] = {
+		['leve'] = 1,
+		['log_file'] = 'log.txt',
+		['log_dir'] = 'logs',
+	},
+	['doc_root'] = 'cgi',
+}
