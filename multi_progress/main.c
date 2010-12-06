f73562af5c7fd21a86ff7de02446c2c543229372
donkeyji <donkeyji@7faa1df5-b907-cf65-c270-3e1877ae6e1a>
2010-12-06 09:05:27 +0000

    调整了event_init的调用位置

--- multi_progress/main.c
+++ multi_progress/main.c
@@ -10,18 +10,17 @@ int main()
 
 	log_init();
 
-	event_init();
+	event_init();//必须先初始化，否则后面有依赖
 
-	signal_init();
+	signal_init();//依赖libevent
 
-	//完成server对象的创建，listen socket的创建，libevent的初始化
-	server_init();
+	server_init();//完成server对象的创建，listen socket创建
 
-	daemon_run();
+	daemon_run();//是否daemon
 
-	create_pidfile();
+	create_pidfile();//创建pid文件
 
-	master_cycle();
+	master_cycle();//master的逻辑
 
 	return 0;
 }
