ce37439592465734fb6aedb3f2f16b116c6aa4f4
donkeyji <donkeyji@7faa1df5-b907-cf65-c270-3e1877ae6e1a>
2010-12-11 12:19:19 +0000

    add protocol description file

--- server_maps/login.c
+++ server_maps/login.c
@@ -7,9 +7,24 @@ static void _login_conn_cb(conn_client* c);
 static void _login_read_cb(conn_client* c);
 static void _login_error_cb(conn_client* c);
 //------------------------------------------
+struct session_tbl* stbl = NULL;//uid为key，存储session
+struct base_info* bif = NULL;//存储各个realm的基本信息
 
-int login_init() 
+
+int 
+login_init() 
 {
+	stbl = session_tbl_new();
+	if (stbl == NULL) {
+		log_error();
+		exit(0);
+	}
+	bif = base_info_new();
+	if (stbl == NULL) {
+		log_error();
+		exit(0);
+	}
+
 	conn_server* s = conn_server_new();
 	s->port = cfg->port;
 	s->conn_cb = _login_conn_cb;
@@ -17,16 +32,20 @@ int login_init()
 	s->conn_error_cb = _login_error_cb;
 	int ret = conn_server_startup(s);
 	if (ret < 0) {
-		return -1;
+		log_error();
+		exit(0);
 	}
 	return 0;
 }
 
-static void _login_conn_cb(conn_client* c)
+static void 
+_login_conn_cb(conn_client* c)
 {}
 
-static void _login_read_cb(conn_client* c)
+static void 
+_login_read_cb(conn_client* c)
 {}
 
-static void _login_error_cb(conn_client* c)
+static void 
+_login_error_cb(conn_client* c)
 {}
