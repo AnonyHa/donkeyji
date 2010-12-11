4da4088e3d9794d48db214ff778ab0a5bbc0d963
donkeyji <donkeyji@7faa1df5-b907-cf65-c270-3e1877ae6e1a>
2010-12-11 08:08:49 +0000

    basic framework for login server

--- server_maps/login.c
+++ server_maps/login.c
@@ -1,4 +1,32 @@
+#include "conn.h"
+#include "login.h"
+
+//------------------------------------------
+//static function declaration
+static void _login_conn_cb(conn_client* c);
+static void _login_read_cb(conn_client* c);
+static void _login_error_cb(conn_client* c);
+//------------------------------------------
+
 int login_init() 
 {
-
+	conn_server* s = conn_server_new();
+	s->port = cfg->port;
+	s->conn_cb = _login_conn_cb;
+	s->conn_read_cb = _login_read_cb;
+	s->conn_error_cb = _login_error_cb;
+	int ret = conn_server_startup(s);
+	if (ret < 0) {
+		return -1;
+	}
+	return 0;
 }
+
+static void _login_conn_cb(conn_client* c)
+{}
+
+static void _login_read_cb(conn_client* c)
+{}
+
+static void _login_error_cb(conn_client* c)
+{}
