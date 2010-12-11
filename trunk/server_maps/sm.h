97092942d99b0196c4dd597d27d0919cb25981f3
donkeyji <donkeyji@7faa1df5-b907-cf65-c270-3e1877ae6e1a>
2010-12-11 07:38:48 +0000

    add sm.h

---
+++ trunk/server_maps/sm.h
@@ -0,0 +1,30 @@
+/*仅仅包含系统文件，至于自定义的.h文件还是各自去处理吧*/
+
+//ansi c
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <signal.h>
+#include <errno.h>
+#include <string.h>
+#include <assert.h>
+#include <time.h>
+
+//linux
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+//lua
+#include <lua5.1/lua.h>
+#include <lua5.1/lualib.h>
+#include <lua5.1/lauxlib.h>
+
+//libevent
+#include <event.h>
+
