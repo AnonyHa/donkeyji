6c4444b6c8092baf5f0ff9475ef8e8caa74ba3bb
donkeyji <donkeyji@7faa1df5-b907-cf65-c270-3e1877ae6e1a>
2010-12-11 07:29:28 +0000

    add new file

---
+++ server_maps/conn.h
@@ -0,0 +1,33 @@
+typedef void (*on_client_conn_cb)(conn* c);
+typedef void (*on_client_read_cb)(conn* c);
+typedef void (*on_client_close_cb)(conn* c);
+
+typedef struct _conn_server
+{
+	int listen_sock; 
+	int port;
+	struct event* listen_ev;
+	on_client_conn_cb conn_cb;
+	on_client_read_cb read_cb;
+	on_client_close_cb close_cb;
+}conn_server;
+
+typedef struct _conn_client
+{
+	int vfd;
+	int sock;
+	struct bufferevent* bev;
+
+	buffer* wbuf;
+	buffer* rbuf;
+}conn_client;
+
+//server的参数需要调用conn_server_new之后设置
+conn_server* conn_server_new();
+void conn_server_free(conn_server* s);
+
+conn_client* conn_client_new();
+void conn_client_free(conn_client* c);
+
+//创建listen sock，开始listen
+int conn_server_start(conn_server* s);
