3c4ba013bc3a6465b976c101e0223374d911e830
donkeyji <donkeyji@7faa1df5-b907-cf65-c270-3e1877ae6e1a>
2010-12-12 15:16:18 +0000

    complete conn.c

--- server_maps/conn.c
+++ server_maps/conn.c
@@ -1,8 +1,8 @@
 #include "conn.h"
 
-conn_server* srv_mgr = NULL; 
-conn_client* clt_mgr = NULL;
-
+//max 32
+static conn_server* srv_mgr[MAX_CONN_SRV];
+static int srv_idx = 0;
 //-----------------------------------------------------------------
 //static function declaration
 static void _conn_server_listen_cb(int fd, short what, void* arg);
@@ -13,7 +13,16 @@ static int _conn_client_startup(conn* c)
 static void _conn_client_read_cb(struct bufferevent* bev, void* arg);
 static void _conn_client_error_cb(strut bufferevent* bev, short what, void* arg);
 //-----------------------------------------------------------------
+int conn_init()
+{
+	int i;
+	for (i=0; i<MAX_CONN_SRV; i++) {
+		srv_mgr[i] = NULL;
+	}
+	return 0;
+}
 
+//-----------------------------------------------------------------
 conn_server* 
 conn_server_new()
 {
@@ -32,43 +41,57 @@ conn_server_new()
 int
 conn_server_startup(conn_server* s)
 {
+	//reach to the max cnt for tcp server
+	if (srv_idx >= MAX_CONN_SRV) {
+		log_error();
+		return -1;
+	}
+	//add to the server mgr
+	s->idx = srv_idx;
+	srv_mgr[srv_idx] = s;
+	srv_idx++;
+
 	s->listen_ev = (struct event*)malloc(sizeof(struct event));
 	assert(s->listen_ev);
 	event_set(s->listen_ev, EV_READ|EV_PERSIST, _conn_server_listen_cb, s);
 	event_add(s->listen_ev, NULL);
+	return 0;
 }
 
-static void 
-_conn_server_listen_cb(int fd, short what, void* arg)
+void
+conn_server_free(conn_server* s)
 {
-	conn_server* s = (conn_server*)arg;
-	struct sockaddr_in addr;
-	int len = sizeof(addr);
-
-	int fd = accept(fd, (struct sockaddr*)&addr, (socklen_t*)&len);
-	if (fd < 0) {
-		log_error();
+	if (s == NULL)
 		return;
-	}
-
-	//to do:...
-	int flag = fcntl(fd, F_GETFL, 0);
-	flag |= O_NONBLOCK;
-	fcntl(fd, F_SETFL, flag);
-
-	conn_client_mgr_add(s->cmgr, fd);
-
-	//call back
-	s->on_conn_cb(c);
-
-	//register READ/WRITE event for client
-	_conn_client_startup(c);
+	if (s->listen_ev != NULL)
+		free(s->listen_ev);
+	if (s->cmgr != NULL)
+		conn_client_mgr_free(s->cmgr);
+	free(s);
 }
 
-void conn_server_free(conn_server* s)
-{}
+int
+conn_server_add_client(conn_server* s, conn_client* c)
+{
+	conn_client_mgr* cm = s->cmgr;
+	c->srv_idx = s->idx;
+	int code = conn_client_mgr_add(cm, c);
+	return code;
+}
 
-conn_client* conn_client_new()
+
+int
+conn_server_del_client(conn_server* s, conn_client* c)
+{
+	conn_client_mgr* cm = s->cmgr;
+	int code = conn_client_mgr_del(cm, c);
+	return code;
+}
+
+
+//---------------------------------------------------------------
+conn_client*
+conn_client_new()
 {
 	conn_client* c = (conn_client*)calloc(1, sizeof(conn_client));
 	assert(c);
@@ -80,20 +103,26 @@ conn_client* conn_client_new()
 	c->rbuf = buffer_new();
 }
 
-void conn_client_free(conn_client* c)
+void
+conn_client_free(conn_client* c)
 {
 	if (c == NULL)
 		return;
-	bufferevent_free(c->bev);
-	buffer_free(c->rbuf);
-	buffer_free(c->wbuf);
+	if (c->bev != NULL)
+		bufferevent_free(c->bev);
+	if (c->rbuf != NULL)
+		buffer_free(c->rbuf);
+	if (c->wbuf != NULL)
+		buffer_free(c->wbuf);
 	free(c);
 }
 
-void conn_client_reset(conn_client* c)
+void
+conn_client_reset(conn_client* c)
 {
 	if (c == NULL)
 		return;
+	c->srv_idx = -1;
 	c->idx = -1;
 	c->sock = -1;
 	bufferevent_disable(c->bev, EV_READ|EV_WRITE);
@@ -101,7 +130,9 @@ void conn_client_reset(conn_client* c)
 	buffer_reset(c->wbuf);
 }
 
-conn_client_mgr* conn_client_mgr_new()
+//--------------------------------------------------------------
+conn_client_mgr*
+conn_client_mgr_new()
 {
 	conn_client_mgr* cm = (conn_client_mgr*)calloc(1, sizeof(conn_client_mgr));
 	assert(cm);
@@ -110,18 +141,24 @@ conn_client_mgr* conn_client_mgr_new()
 	cm->used = 0;
 }
 
-void conn_client_mgr_free(conn_client_mgr* cm)
+void
+conn_client_mgr_free(conn_client_mgr* cm)
 {
+	int i;
 	if (cm == NULL)
 		return;
-	int i;
-	for (i=0; i<size; i++)
-		conn_client_free(cm->ptr[i]);
-	free(cm->ptr);
+	if (cm->ptr != NULL) {
+		for (i=0; i<size; i++) {
+			if (cm->ptr[i] != NULL)
+				conn_client_free(cm->ptr[i]);
+		}
+		free(cm->ptr);
+	}
 	free(cm);
 }
 
-void conn_client_mgr_reset(conn_client_mgr* cm)
+void
+conn_client_mgr_reset(conn_client_mgr* cm)
 {
 	if (cm ==NULL)
 		return;
@@ -131,9 +168,11 @@ void conn_client_mgr_reset(conn_client_mgr* cm)
 	cm->used = 0;
 }
 
-int conn_client_mgr_add(conn_client_mgr* cm, int sock)
+int
+conn_client_mgr_add(conn_client_mgr* cm, int sock)
 {
 	int i;
+
 	if (cm->size == 0) {
 		cm->size += 128;
 		cm->ptr = (conn_client**)calloc(cm->size, sizeof(conn_client*));
@@ -158,9 +197,11 @@ int conn_client_mgr_add(conn_client_mgr* cm, int sock)
 	return 0;
 }
 
-int conn_client_mgr_del(conn_client_mgr* cm, conn_client* c)
+int
+conn_client_mgr_del(conn_client_mgr* cm, conn_client* c)
 {
 	conn_client* tmp;
+
 	if (c->idx == -1)
 		return 0;
 	if (c->idx == cm->used-1) {
@@ -175,7 +216,10 @@ int conn_client_mgr_del(conn_client_mgr* cm, conn_client* c)
 	cm->used--;
 	return 0;
 }
-//------------------------------------------------------------------------
+
+//----------------------------------------------------------------
+//static function
+//----------------------------------------------------------------
 static int 
 _conn_client_startup(conn* c)
 {
@@ -186,8 +230,65 @@ _conn_client_startup(conn* c)
 
 static void 
 _conn_client_read_cb(struct bufferevent* bev, void* arg)
-{}
+{
+	size_t len = bev->input->off;
+	if (len == 0) {
+		return;
+	}
+
+	conn_client* c = (conn_client*)arg;
+	char buf[len];
+	bzero(buf, len);
+
+	size_t read_len = bufferevent_read(bev, buf, len);
+	if (read_len <= 0) {
+		return;
+	}
+
+	buffer_append(c->rbuf, buf, read_len);
+
+	conn_server* s = srv_mgr[c->srv_idx];
+
+	//invoke callback, to do what the server like to do
+	(s->read_cb)(c);
+}
 
 static void 
 _conn_client_error_cb(strut bufferevent* bev, short what, void* arg)
-{}
+{
+	conn_client* c = (conn_client*)arg;
+
+	conn_server* s = srv_mgr[c->srv_idx];
+
+	(s->error_cb)(c);//must invoked before del client
+
+	conn_server_del_client(s, c);
+}
+
+static void 
+_conn_server_listen_cb(int fd, short what, void* arg)
+{
+	conn_server* s = (conn_server*)arg;
+	struct sockaddr_in addr;
+	int len = sizeof(addr);
+
+	int fd = accept(fd, (struct sockaddr*)&addr, (socklen_t*)&len);
+	if (fd < 0) {
+		log_error();
+		return;
+	}
+
+	//to do:...
+	int flag = fcntl(fd, F_GETFL, 0);
+	flag |= O_NONBLOCK;
+	fcntl(fd, F_SETFL, flag);
+
+	//so nasty .......
+	conn_client_mgr_add(s, fd);
+
+	//call back
+	(s->conn_cb)(c);
+
+	//register READ/WRITE event for client
+	_conn_client_startup(c);
+}
