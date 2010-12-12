3a74e70bb943a861e715b11ed837fe14f88b0945
donkeyji <donkeyji@7faa1df5-b907-cf65-c270-3e1877ae6e1a>
2010-12-12 14:11:48 +0000

    conn implement

--- server_maps/conn.c
+++ server_maps/conn.c
@@ -17,14 +17,23 @@ static void _conn_client_error_cb(strut bufferevent* bev, short what, void* arg)
 conn_server* 
 conn_server_new()
 {
+	//use calloc to set the memory to zero
 	conn_server* s = (conn_server*)calloc(1, sizeof(conn_server));
 	assert(s);
+	s->listen_ev = NULL;
+	s->listen_sock = -1;
+	s->port = -1;
+	s->conn_cb = NULL;
+	s->read_cb = NULL;
+	s->error_cb = NULL;
 	return s;
 }
 
-int 
+int
 conn_server_startup(conn_server* s)
 {
+	s->listen_ev = (struct event*)malloc(sizeof(struct event));
+	assert(s->listen_ev);
 	event_set(s->listen_ev, EV_READ|EV_PERSIST, _conn_server_listen_cb, s);
 	event_add(s->listen_ev, NULL);
 }
@@ -33,14 +42,21 @@ static void
 _conn_server_listen_cb(int fd, short what, void* arg)
 {
 	conn_server* s = (conn_server*)arg;
+	struct sockaddr_in addr;
+	int len = sizeof(addr);
 
-	int fd = accept(fd);
+	int fd = accept(fd, (struct sockaddr*)&addr, (socklen_t*)&len);
+	if (fd < 0) {
+		log_error();
+		return;
+	}
 
 	//to do:...
-	set_nonblocking(fd);
+	int flag = fcntl(fd, F_GETFL, 0);
+	flag |= O_NONBLOCK;
+	fcntl(fd, F_SETFL, flag);
 
-	conn_client* c = conn_client_new();
-	c->sock = fd;
+	conn_client_mgr_add(s->cmgr, fd);
 
 	//call back
 	s->on_conn_cb(c);
@@ -49,11 +65,122 @@ _conn_server_listen_cb(int fd, short what, void* arg)
 	_conn_client_startup(c);
 }
 
+void conn_server_free(conn_server* s)
+{}
+
+conn_client* conn_client_new()
+{
+	conn_client* c = (conn_client*)calloc(1, sizeof(conn_client));
+	assert(c);
+	c->idx = -1;
+	c->sock = -1;
+	c->bev = NULL;
+
+	c->wbuf = buffer_new();
+	c->rbuf = buffer_new();
+}
+
+void conn_client_free(conn_client* c)
+{
+	if (c == NULL)
+		return;
+	bufferevent_free(c->bev);
+	buffer_free(c->rbuf);
+	buffer_free(c->wbuf);
+	free(c);
+}
+
+void conn_client_reset(conn_client* c)
+{
+	if (c == NULL)
+		return;
+	c->idx = -1;
+	c->sock = -1;
+	bufferevent_disable(c->bev, EV_READ|EV_WRITE);
+	buffer_reset(c->rbuf);
+	buffer_reset(c->wbuf);
+}
+
+conn_client_mgr* conn_client_mgr_new()
+{
+	conn_client_mgr* cm = (conn_client_mgr*)calloc(1, sizeof(conn_client_mgr));
+	assert(cm);
+	cm->ptr = NULL;
+	cm->size = 0;
+	cm->used = 0;
+}
+
+void conn_client_mgr_free(conn_client_mgr* cm)
+{
+	if (cm == NULL)
+		return;
+	int i;
+	for (i=0; i<size; i++)
+		conn_client_free(cm->ptr[i]);
+	free(cm->ptr);
+	free(cm);
+}
+
+void conn_client_mgr_reset(conn_client_mgr* cm)
+{
+	if (cm ==NULL)
+		return;
+	int i;
+	for (i=0; i<cm->used; i++)
+		conn_client_reset(cm->ptr[i]);
+	cm->used = 0;
+}
+
+int conn_client_mgr_add(conn_client_mgr* cm, int sock)
+{
+	int i;
+	if (cm->size == 0) {
+		cm->size += 128;
+		cm->ptr = (conn_client**)calloc(cm->size, sizeof(conn_client*));
+		for (i=0; i<cm->size; i++) {
+			cm->ptr[i] = conn_client_new();
+		} 
+	} else if (cm->size == cm->used) {
+		cm->size += 128;
+		cm->ptr = (conn_client**)realloc(cm->ptr, cm->size * sizeof(conn_client*));
+		for (i=cm->used; i<cm->size; i++) {
+			cm->ptr[i] = conn_client_new();
+		}
+	}
+
+	conn_client* c = conn_client_new();
+	c->sock = sock;
+	c->idx = cm->used;
+
+	cm->ptr[cm->used] = c;
+	cm->used++;
+
+	return 0;
+}
+
+int conn_client_mgr_del(conn_client_mgr* cm, conn_client* c)
+{
+	conn_client* tmp;
+	if (c->idx == -1)
+		return 0;
+	if (c->idx == cm->used-1) {
+		conn_client_reset(cm->ptr[cm->used]);
+	} else {
+		tmp = c;	
+		cm->ptr[c->idx] = cm->ptr[cm->used-1];
+		cm->ptr[c->idx]->idx = c->idx;
+		cm->ptr[cm->used-1] = tmp;
+		conn_client_reset(cm->ptr[cm->used-1]);
+	}
+	cm->used--;
+	return 0;
+}
+//------------------------------------------------------------------------
 static int 
 _conn_client_startup(conn* c)
 {
 	c->bev = bufferevent_new(c->fd, _conn_client_read_cb, NULL, _conn_client_error_cb, (void*)c);
-	bufferevent_enable(c->bev);
+	bufferevent_enable(c->bev, EV_READ|EV_WRITE);
 	return 0;
 }
 
--- server_maps/conn.h
+++ server_maps/conn.h
@@ -7,6 +7,7 @@ typedef struct _conn_server
 	int listen_sock; 
 	int port;
 	struct event* listen_ev;
+	conn_client_mgr* cmgr;
 	on_client_conn_cb conn_cb;
 	on_client_read_cb read_cb;
 	on_client_error_cb error_cb;
@@ -14,13 +15,20 @@ typedef struct _conn_server
 
 typedef struct _conn_client
 {
-	int vfd;
+	int idx;//index in conn_client_mgr
 	int sock;
 	struct bufferevent* bev;
 
 	buffer* wbuf;
 	buffer* rbuf;
 }conn_client;
+
+typedef struct _conn_client_mgr
+{
+	conn_client** ptr;
+	size_t size;
+	size_t used;
+}conn_client_mgr;
 
 //server的参数需要调用conn_server_new之后设置
 conn_server* conn_server_new();
@@ -31,3 +39,8 @@ void conn_client_free(conn_client* c);
 
 //创建listen sock，开始listen
 int conn_server_startup(conn_server* s);
+
+conn_client_mgr* conn_client_mgr_new();
+void conn_client_mgr_free(conn_client_mgr* cm);
+int conn_client_mgr_add(conn_client_mgr* cm, int sock); 
+int conn_client_mgr_del(conn_client* cm, conn_client* c);
