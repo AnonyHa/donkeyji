93379e897b92b37a12b51f0854b4240981bc695b
donkeyji <donkeyji@7faa1df5-b907-cf65-c270-3e1877ae6e1a>
2010-12-12 15:16:18 +0000

    complete conn.c

--- trunk/server_maps/conn.h
+++ trunk/server_maps/conn.h
@@ -1,17 +1,8 @@
+#define MAX_CONN_SRV 32
+
 typedef void (*on_client_conn_cb)(conn* c);
 typedef void (*on_client_read_cb)(conn* c);
 typedef void (*on_client_error_cb)(conn* c);
-
-typedef struct _conn_server
-{
-	int listen_sock; 
-	int port;
-	struct event* listen_ev;
-	conn_client_mgr* cmgr;
-	on_client_conn_cb conn_cb;
-	on_client_read_cb read_cb;
-	on_client_error_cb error_cb;
-}conn_server;
 
 typedef struct _conn_client
 {
@@ -30,17 +21,44 @@ typedef struct _conn_client_mgr
 	size_t used;
 }conn_client_mgr;
 
+typedef struct _conn_server
+{
+	int srv_idx;
+	int idx;
+	int listen_sock; 
+	int port;
+	struct event* listen_ev;
+	conn_client_mgr* cmgr;
+	on_client_conn_cb conn_cb;
+	on_client_read_cb read_cb;
+	on_client_error_cb error_cb;
+}conn_server;
+
+
+//---------------------------------------------
 //server的参数需要调用conn_server_new之后设置
 conn_server* conn_server_new();
 void conn_server_free(conn_server* s);
-
-conn_client* conn_client_new();
-void conn_client_free(conn_client* c);
-
 //创建listen sock，开始listen
 int conn_server_startup(conn_server* s);
+//---------------------------------------------
 
+
+//---------------------------------------------
+conn_client* conn_client_new();
+void conn_client_free(conn_client* c);
+void conn_client_reset(conn_client* c);
+//---------------------------------------------
+
+
+//---------------------------------------------
 conn_client_mgr* conn_client_mgr_new();
 void conn_client_mgr_free(conn_client_mgr* cm);
 int conn_client_mgr_add(conn_client_mgr* cm, int sock); 
 int conn_client_mgr_del(conn_client* cm, conn_client* c);
+//---------------------------------------------
+
+
+//---------------------------------------------
+int conn_init();
+//---------------------------------------------
