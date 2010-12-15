40f6de956d838cd1935204430854449c7e069b6b
donkeyji <donkeyji@7faa1df5-b907-cf65-c270-3e1877ae6e1a>
2010-12-15 05:58:34 +0000

    修改了Makefile，针对C源文件，非cpp文件

--- trunk/Test/test.py
+++ trunk/Test/test.py
@@ -1,12 +1,5 @@
-import gevent
-from gevent import socket
-from gevent import core
+import urllib
 
-def f(ev, type):
-	print '88888'
-
-core.init()
-
-#core.dispatch()
-core.loop()
-print '00000000000'
+fd = urllib.urlopen('http://hi.163.com/simpleapi/game/role/get_token?game=itown')
+rsp = fd.read()
+print rsp
