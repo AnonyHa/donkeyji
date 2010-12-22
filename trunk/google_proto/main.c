71c24a12cc9fb000e1caf0670c580745765ddbae
donkeyji <donkeyji@7faa1df5-b907-cf65-c270-3e1877ae6e1a>
2010-12-22 10:22:03 +0000

    google protocol test

---
+++ trunk/google_proto/main.c
@@ -0,0 +1,14 @@
+#include <time.h>
+
+extern int reg_msg();
+
+int main()
+{
+	reg_msg();
+	/*
+	while (1) {
+		sleep(1);
+	}
+	*/
+	return 0;
+}
---
+++ trunk/google_proto/pto_macro.h
@@ -0,0 +1,6 @@
+#define C_BUY_ITEM_T c_buy_item
+#define C_BUY_ITEM_ID 1
+#define S_BUY_ITEM_T s_buy_item
+#define S_BUY_ITEM_ID 2
+#define S_LOGIN_T s_login
+#define S_LOGIN_ID 3
