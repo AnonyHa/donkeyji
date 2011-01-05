4e60c756e5ca39e228776f8f2ad50a33627d1925
donkeyji <donkeyji@7faa1df5-b907-cf65-c270-3e1877ae6e1a>
2011-01-05 06:14:04 +0000

    modify Makefile

--- Test/mod.c
+++ Test/mod.c
@@ -26,9 +26,92 @@ static int test(lua_State* L)
 	return 0;
 }
 
+static int _f(lua_State* L)
+{
+	luaL_error(L, "in _f");
+	return 0;
+}
+
+static int new_t(lua_State* L)
+{
+	//int top = lua_gettop(L);
+	lua_newtable(L);
+	lua_pushstring(L, "one");
+	lua_pushinteger(L, 5);
+	lua_settable(L, -3);
+	printf("--------\n");
+	return 1;
+}
+
+static int obj_len(lua_State* L)
+{
+	int len = lua_objlen(L, -1);
+	printf("len = %d\n", len);
+	int i;
+	for (i=1; i<=len; i++) {
+		lua_rawgeti(L, -1, i);
+		printf("val = %d\n", lua_tointeger(L, -1));
+		printf("type = %s\n", lua_typename(L, lua_type(L, -1)));
+		lua_pop(L, 1);
+	}
+	return 0;
+}
+
+static int travel(lua_State* L)
+{
+	int top = lua_gettop(L);
+	if (!lua_istable(L, -1)) {
+		luaL_typerror(L, -1, "table");
+		printf("*******\n");
+		return 0;
+	}
+	lua_pushnil(L);
+	int r[2];
+	int i = 0;
+	while (lua_next(L, -2) != 0) {
+		printf("%s---%s\n", 
+			lua_typename(L, lua_type(L, -2)),
+			lua_typename(L, lua_type(L, -1))
+		);
+		printf("key = %s\n", lua_tostring(L, -2));
+		//lua_pop(L, 1);
+		lua_pushvalue(L, -2);
+		r[i] = luaL_ref(L, LUA_REGISTRYINDEX);
+		i++;
+		lua_pop(L, 1);
+	}
+	for (i=0; i<2; i++) {
+		lua_rawgeti(L, LUA_REGISTRYINDEX, r[i]);
+		printf("---%s\n", lua_typename(L, lua_type(L, -1)));
+		lua_pop(L, 1);
+	}
+
+	lua_settop(L, top);
+	return 0;
+}
+
+static int new_env(lua_State* L)
+{
+	int top = lua_gettop(L);
+	lua_getglobal(L, "f");
+	if (!lua_isfunction(L, -1)) {
+		lua_settop(L, top);
+		luaL_error(L, "not function");
+	}
+	lua_newtable(L);
+	int ret = lua_setfenv(L, -2);
+	printf("------ret = %d\n", ret);
+	lua_pcall(L, 0, 0, 0);
+	return 0;
+}
+
 // ---------------------------------------
 static const struct luaL_Reg hujilib[] = {
 	{"test", test},
+	{"new_t", new_t},
+	{"obj_len", obj_len},
+	{"travel", travel},
+	{"new_env", new_env},
 	{NULL, NULL}
 };
 
--- Test/test.py
+++ Test/test.py
@@ -1,5 +1,47 @@
 import urllib
+import urllib2
 
-fd = urllib.urlopen('http://hi.163.com/simpleapi/game/role/get_token?game=itown')
-rsp = fd.read()
-print rsp
+URL = 'http://192.168.10.121:8078/simpleapi/game/role'
+
+# ------------------------------------------------
+# to do: how to check the validity of the argument
+# ------------------------------------------------
+
+def handle_friend():
+	data = {
+		'game' : 'itown',
+		#'regionid' : 1,
+		#'serverid' : 2,
+		#'roleid' : 124,
+		'rolename' : 'donitz',
+		#'friend_regionid' : 1,
+		#'friend_serverid' : 2,
+		#'friend_roleid' : 125,
+		'token' : '4eelZS24yHUJG9UXmLw5Yklrluh1A3FhHcQwkBZV',
+		#'sinceid' : 1,
+		'content' : 'i am donitz',
+		#'comment_thread' : 2,
+	}
+	#init_url = '%s/friendship/create' % URL
+	#init_url = '%s/action/timeline' % URL
+	#init_url = '%s/action/homeline' % URL
+	init_url = '%s/record/create' % URL
+	#init_url = '%s/comment/list' % URL
+	print init_url
+	arg = urllib.urlencode(data)
+	req = urllib2.Request(init_url, arg)
+	rsp = urllib2.urlopen(req)
+	code = rsp.code
+	print 'code = ', code
+	if code == 200:
+		return {'code' : 0}
+	else:
+		return {'code' : -1}
+
+#-----------------------------------------------------
+#-----------------------------------------------------
+
+
+if __name__ == '__main__':
+	ret = handle_friend()
+	print ret
