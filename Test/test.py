bd26ef7368026cb777c3d4d2b552116a2cbcc2ee
donkeyji <donkeyji@7faa1df5-b907-cf65-c270-3e1877ae6e1a>
2011-05-24 08:04:36 +0000

    add cpp dir

--- Test/test.py
+++ Test/test.py
@@ -1,9 +1,37 @@
 #coding:gbk
-import a
+import traceback
 
-game_name = "连连看"
-desc = "很好的成就"
-content = "获得了#c9BFF52%s#n的#c00B6FF%s#n成就" % (game_name, desc)
-content = '%s%s%s' % (a.a, a.b, content)
-content = content.decode('gbk').encode('utf-8')
-print content
+def step_1():
+	raise '111'
+
+def step_2():
+	raise '222'
+
+def step_3():
+	pass
+
+def do_test():
+	try:
+		step_1()# raise inside
+	except:
+		raise
+
+	try:
+		step_2()# raise inside
+	except:
+		pass
+
+	step_3()# no raise inside
+
+def init_test():
+	raise 'init'
+
+if __name__ == "__main__":
+	init_test()# if raise, exit directly
+
+	while 1:# if raise, donot exit, just catch the exception
+		try:
+			do_test()
+		except:
+			pass
+		eleep(1)
