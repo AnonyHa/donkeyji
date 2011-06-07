f6b051d2b80b70ff35a1f60e32252bcd8b0e6cf8
donkeyji <donkeyji@7faa1df5-b907-cf65-c270-3e1877ae6e1a>
2011-06-07 06:38:05 +0000

    add more argument

--- mykdig.py
+++ mykdig.py
@@ -5,7 +5,7 @@ from optparse import OptionParser
 
 __description__ = ''
 __version__ = ''
-__usage__ = 'usage: python mykdig.py inputfile outputfile'
+__usage__ = 'usage: python mykdig.py inputfile outputfile sort_key'
 
 parser = OptionParser(description=__description__, usage=__usage__, version=__version__)
 #parser.add_option('-v', '--verbose', 
@@ -18,33 +18,37 @@ parser = OptionParser(description=__description__, usage=__usage__, version=__ve
 options = None
 args = []
 
-def do_kdig(infile, outfile):
+def do_kdig(infile, outfile, sort_key):
 	result = []
+
 	f = open(infile, 'r')
 	for line in f:
 		items = line.rstrip('\n').split(',')
-		#print type(items)
 		result.append(items)
 
-	result.sort(key=lambda d:float(d[1]))
-	#print result
+	if sort_key not in (1, 2, 3):
+		print 'sort key should in in (1, 2, 3)'
+		sys.exit(1)
+
+	result.sort(key=lambda d:float(d[sort_key]))
 
 	fo = file(outfile, 'w')
 	for items in result:
-		fo.write(','.join(items))
-		fo.write('\n')
+		fo.write(','.join(items) + '\n')
 	
 
 if __name__ == '__main__':
 	options, args = parser.parse_args()
-	print options
+	#print options
 
-	print args
+	#print args
 
-	if len(args) < 2:
+	if len(args) < 3:
 		parser.print_help()
 		sys.exit(1)
 
 	infile = args[0]# input file
 	outfile = args[1]# output file
-	do_kdig(infile, outfile)
+	sort_key = int(args[2])# sort key
+
+	do_kdig(infile, outfile, sort_key)
