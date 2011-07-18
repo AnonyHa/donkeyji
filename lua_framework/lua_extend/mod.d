5454d795099567a740dbc1f13bb595cfc9462cbb
donkeyji <donkeyji@7faa1df5-b907-cf65-c270-3e1877ae6e1a>
2011-07-18 05:55:28 +0000

    add lua_extend directory

---
+++ lua_framework/lua_extend/mod.d
@@ -0,0 +1,5 @@
+mod.o: lua_extend/mod.c D:/Program\ Files/Lua/5.1/include/lua.h \
+  D:/Program\ Files/Lua/5.1/include/luaconf.h \
+  D:/Program\ Files/Lua/5.1/include/lauxlib.h \
+  D:/Program\ Files/Lua/5.1/include/lua.h \
+  D:/Program\ Files/Lua/5.1/include/lualib.h
