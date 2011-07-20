0c60ca24ba77c4523a79dd5d5c2e0159a9f2adc0
donkeyji <donkeyji@7faa1df5-b907-cf65-c270-3e1877ae6e1a>
2011-07-20 03:21:21 +0000

    添加带trace的lua_pcall

--- lua_framework/main.c
+++ lua_framework/main.c
@@ -10,6 +10,28 @@ extern "C" {
 
 int luaopen_hujilib(lua_State*);
 //-----------------------------------------
+int traceback(lua_State* L)
+{
+	printf("====%s\n", lua_tostring(L, 1));
+	/*
+	lua_getglobal(L, "trace");
+	lua_pushvalue(L, 1);
+	lua_pcall(L, 1, 0, 0);
+	*/
+	return 1;
+}
+
+int mypcall(lua_State* L, int nargs, int nrets, int errfunc)
+{
+	int base = lua_gettop(L) - nargs;
+	lua_pushcfunction(L, traceback);
+	lua_insert(L, base);
+	int status = lua_pcall(L, nargs, nrets, base);
+	lua_remove(L, base);
+	return status;
+}
+
+
 int main()
 {
 	lua_State* L = luaL_newstate();
@@ -21,6 +43,15 @@ int main()
 		printf("[error]");
 		fprintf(stderr, "%s\n", lua_tostring(L, -1));//取栈顶的错误信息
 		lua_pop(L, 1);
+		return -1;
+	}
+
+	lua_getglobal(L, "huge");
+	lua_pushinteger(L, 33);
+	error = mypcall(L, 1, 0, 0);
+	if (error) {
+		printf("[error]");
+		return -1;
 	}
 
 	lua_close(L);
