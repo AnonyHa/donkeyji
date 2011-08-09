7c2a02fbb0c5fc735b4abec87ff051fdcfae9b1b
donkeyji <donkeyji@7faa1df5-b907-cf65-c270-3e1877ae6e1a>
2011-08-09 12:38:57 +0000

    add file

---
+++ trunk/rank/main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include "conf.h"
+#include "log.h"
+
+int set_pwd()
+{}
+
+int sig_init()
+{}
+
+int set_rlimit()
+{}
+
+int main(int argc, char** argv)
+{
+	/*
+	char* conf_file = NULL;
+	char* log_file = NULL;
+	int srv_mode = -1;
+	char c;
+
+	while ((c = getopt(argc, argv, "f:m:l:")) != -1) {
+		switch (c) {
+		case 'f':
+			break;
+		case 'm':
+			break;
+		case 'l':
+			break;
+		default:
+			break;
+		}
+	}
+
+	conf_init();
+
+	if (cfg->is_daemon) {
+		daemon(1, 1);
+	}
+
+	set_pwd();
+
+	log_init();
+	sig_init();
+	set_rlimit();
+	mem_init();
+
+	rank_init();
+
+	event_init();
+
+	svr_init();
+
+	event_dispatch();
+
+	*/
+	return 0;
+}
+
