d53156858e1fbdd183f64c29e2fc1f3b2caf7459
donkeyji <donkeyji@7faa1df5-b907-cf65-c270-3e1877ae6e1a>
2012-01-15 14:11:43 +0000

    modify models

--- dwitter/dwitter/account/models.py
+++ dwitter/dwitter/account/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class Role(models.Model):
-	role_name = models.CharField(max_length=32, unique=True)
-
 class User(models.Model):
 	user_name = models.CharField(max_length=32, unique=True)
 	nickname = models.CharField(max_length=32)
+	follow_cnt = models.IntegerField()
+	following_cnt = models.IntegerField()
+	dwtter_cnt = models.IntegerField()
