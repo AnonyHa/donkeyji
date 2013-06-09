8e417efe424323173e74a67feb88ed6b9f394443
donkeyji <donkeyji@7faa1df5-b907-cf65-c270-3e1877ae6e1a>
2013-06-09 05:19:35 +0000

    add php

--- trunk/shell/debian.sh
+++ trunk/shell/debian.sh
@@ -5,9 +5,19 @@
 #mount -t iso9660 -o loop /mnt/hgfs/MacInstall/debian-6.0.6-amd64-DVD-3.iso /mnt/iso/iso3
 #mount -t iso9660 -o loop /mnt/hgfs/MacInstall/debian-6.0.6-amd64-DVD-4.iso /mnt/iso/iso4
 
-src_prefix=/mnt/hgfs/MacInstall/debian-6.0.6-amd64-DVD-
+src_prefix=/mnt/hgfs/Core/debian-6.0.7-amd64-DVD-
 dst_prefix=/mnt/iso/iso
-for ((i=1;i<9;i++))
+total=9
+for ((i=1;i<$total;i++))
 do
 	mount -t iso9660 -o loop $src_prefix$i.iso $dst_prefix$i
+done
+
+
+src_prefix_src=/mnt/hgfs/Core/debian-6.0.7-source-DVD-
+dst_prefix_src=/mnt/iso/src
+total_src=7
+for ((i=1;i<$total_src;i++))
+do
+	mount -t iso9660 -o loop $src_prefix_src$i.iso $dst_prefix_src$i
 done
