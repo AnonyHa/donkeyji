40e1397c8e1c0a139be851b04b1beb3140204c6b
donkeyji <donkeyji@7faa1df5-b907-cf65-c270-3e1877ae6e1a>
2013-10-23 02:14:15 +0000

    add rm-yum.sh

---
+++ trunk/shell/rm-yum.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#author: huge
+#Sat Mar  9 00:32:35 EST 2013
+
+ERR_ARG=1
+
+arg_cnt=$#
+if [ arg_cnt -ne 1 ]
+then
+	echo 'only one argument is needed'
+	exit $ERR_ARG
+fi
+
+install_file=$1
+failed_file=./failed.log
+succeed_file=./succeed.log
+cmd='yum'
+opt='-y'
+action='remove'
+
+function auto_install () {
+	pack=$1
+	$cmd $opt $action $pack
+}
+
+if [ -e $failed_file ]
+then
+	rm -f $failed_file
+fi
+
+if [ -e $succeed_file ]
+then
+	rm -f $succeed_file
+fi
+
+tmp_list=./_to_install
+grep -v ^# $install_file|grep -v ^# > $tmp_list
+
+while read package
+do
+	echo ==============
+	echo $package
+	echo ==============
+	#apt-get -y --force-yes install $package
+	#$cmd $opt $action $package
+	auto_install $package
+	if [ $? -ne 0 ]
+	then
+		echo $package >> $failed_file
+		#exit $?
+	else
+		echo $package >> $succeed_file
+	fi
+	echo 
+	echo 
+done < $tmp_list
+
+#$cmd $opt --purge autoremove
+#$cmd $opt clean
