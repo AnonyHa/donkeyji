1da3e151ab18d567cc9bac2d0b9bd80f8207433a
donkeyji <donkeyji@7faa1df5-b907-cf65-c270-3e1877ae6e1a>
2013-10-30 09:24:55 +0000

    modify centos.sh

--- shell/centos.sh
+++ shell/centos.sh
@@ -4,5 +4,6 @@ src_prefix=/mnt/hgfs/Core/CentOS-5.9-x86_64-bin-DVD-
 dst_prefix=/mnt/iso/iso
 for ((i=1;i<3;i++))
 do
+	umount $dst_prefix$i
 	mount -t iso9660 -o loop $src_prefix${i}of2.iso $dst_prefix$i
 done
