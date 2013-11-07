bc82e5b01e75d99e3ec37b5d0fb3bf54b6030b7a
donkeyji <donkeyji@7faa1df5-b907-cf65-c270-3e1877ae6e1a>
2013-11-07 04:35:02 +0000

    modify lvs

--- trunk/shell/lvs-dr/real-conf.sh
+++ trunk/shell/lvs-dr/real-conf.sh
@@ -11,8 +11,8 @@ echo 1 > /proc/sys/net/ipv4/conf/all/arp_ignore
 echo 2 > /proc/sys/net/ipv4/conf/lo/arp_announce
 echo 2 > /proc/sys/net/ipv4/conf/all/arp_announce
 
-ifconfig lo:0 192.168.100.1 netmask 255.255.255.255 broadcast 192.168.100.1 up
+ifconfig lo:0 192.168.196.1 netmask 255.255.255.255 broadcast 192.168.196.1 up
 
-route add -host 192.168.100.1 dev lo:0
+route add -host 192.168.196.1 dev lo:0
 
-ifconfig eth0 $REAL_IP netmask 255.255.255.0 broadcast 192.168.100.255 up
+ifconfig eth0 $REAL_IP netmask 255.255.255.0 broadcast 192.168.196.255 gateway 192.168.196.2 up
